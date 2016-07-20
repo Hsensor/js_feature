1e4b9b8d6cbeb1f5d2e7760df7aed8b1e6e9b6d5
Hsensor <1203460788@qq.com>
2016-07-20 13:55:30 +0800

    generator

---
+++ generator_thunk_wrap.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var thunkify = require('thunkify');
+var readFile = thunkify(fs.readFile);
+
+function run(fn){
+	var gen = fn();
+	function next(err,data){
+		if (err) throw err;
+		var result = gen.next(data);
+		if(result.done){
+			return ;
+		}
+		result.value(next);
+	}
+	next();
+}
+
+var gen = function*(){
+	var f1 = yield readFile('./fetch_generator.js');
+	var f2 = yield readFile('./generator.js');
+	var f3 = yield readFile('./README.md');
+	var f4 = yield readFile('./thunk.js');	
+}
+
+run(gen);
